c46eb903b06108e38e5d6cee7a78a5d9a96049cd
BK2004 <thunder10.bk@gmail.com>
2023-11-05 06:28:00 -0600

    Synchronized form UIs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ function NavBar() {
     return (
         <nav className="w-full bg-sky-800 text-white">
             <img src="mortgauge.svg" className="absolute inline-block p-2 pointer-events-none" />
-            <div className="inline-block grid grid-cols-3 text-center font-bold">
+            <div className="grid grid-cols-3 text-center font-bold">
                 <a href="" className="py-3 bg-sky-800 hover:bg-sky-700 transition">MortGauge</a>
                 <a href="/individual" className="py-3 bg-sky-800 hover:bg-sky-700 transition">Will I be approved?</a>
                 <a href="/aggregate" className="py-3 bg-sky-800 hover:bg-sky-700 transition">Aggregate</a>
