02d4a674f21135d343f09ed1dd4cb560f193b7a8
Connor Harris <CWHarris0812@gmail.com>
2023-11-05 11:23:50 -0600

    built homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import AggregateForm from "./components/AggregateForm.jsx";
 import IndividualAnalysisForm from "./components/IndividualAnalysisForm.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
-import Results from "./components/Results.js";
+import Results from "./components/Results.jsx";
+import Home from "./components/Home.jsx";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -10,7 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<div>hello</div>} />
+          <Route index element={<Home />} />
           <Route path="aggregate" element={<AggregateForm />} />
           <Route path="individual" element={<IndividualAnalysisForm />} />
           <Route path="individual/results" element={<Results />} />
