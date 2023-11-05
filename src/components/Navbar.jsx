30f44c126d12b769a3f19c348e7a4dababce7464
Joe <joesu2005@gmail.com>
2023-11-04 20:56:16 -0500

    navbar formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const navBar = () => {
     return (
-        <nav>
+        <nav className="flex w-full justify-around">
             <a href="">MortGauge</a>
             <a href="">Will I be approved?</a>
             <a href="">Other</a>
