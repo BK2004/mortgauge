53b28693013f3937388594c664369b8f33a7e30c
xarkenz <seanedwardsclarke@gmail.com>
2023-11-05 05:47:28 -0600

    stylized navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-const navBar = () => {
+function NavBar() {
     return (
-        <nav className="flex w-full justify-around">
-            <a href="">MortGauge</a>
-            <a href="">Will I be approved?</a>
-            <a href="">Other</a>
+        <nav className="w-full bg-sky-800 text-white">
+            <img src="mortgauge.svg" className="absolute inline-block p-2 pointer-events-none" />
+            <div className="inline-block grid grid-cols-3 text-center font-bold">
+                <a href="" className="py-3 bg-sky-800 hover:bg-sky-700 transition">MortGauge</a>
+                <a href="" className="py-3 bg-sky-800 hover:bg-sky-700 transition">Will I be approved?</a>
+                <a href="" className="py-3 bg-sky-800 hover:bg-sky-700 transition">Other</a>
+            </div>
         </nav>
     )
 }
 
-export default navBar;
+export default NavBar;
