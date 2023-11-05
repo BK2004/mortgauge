1a931c569fe7c5ed30acc4348d0548b0c13448ed
Joe <joesu2005@gmail.com>
2023-11-04 20:49:35 -0500

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const navBar = () => {
+    return (
+        <nav>
+            <a href="">MortGauge</a>
+            <a href="">Will I be approved?</a>
+            <a href="">Other</a>
+        </nav>
+    )
+}
+
+export default navBar;
