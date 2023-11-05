75dd29b5c6c3db85246e2aa2907df281dae6ee70
Joe <joesu2005@gmail.com>
2023-11-05 02:26:24 -0600

    made navbar look slightly nicer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 const navBar = () => {
     return (
-        <nav className="flex w-full justify-around">
-            <a href="">MortGauge</a>
+        <nav className="flex w-full justify-between mt-1 mb-1 px-[100px]">
+            <a href="" className="text-left">MortGauge</a>
             <a href="">Will I be approved?</a>
-            <a href="">Other</a>
+            <a href="" className="text-right">Other</a>
         </nav>
     )
 }
